17e892b1d53003fe685d26378e31a3e959e3b660
Soll <caiorlima22@gmail.com>
2023-03-02 11:51:34 -0300

    Acrementado as telas ainda sem alteraçoes

---
+++ app/src/main/java/com/example/aulaspdm/Logar.java
@@ -0,0 +1,14 @@
+package com.example.aulaspdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Logar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logar);
+    }
+}
