41ec56de2d11c2b1ed9581e049ef8fdee45b0b48
Sollarium <caiorlima22@gmailc.om>
2023-03-09 11:46:56 -0300

    Adicionado a activity calBasi e moficado nomes, e interface basica da mesma

--- app/src/main/java/com/example/aulaspdm/CalBasi.java
+++ app/src/main/java/com/example/aulaspdm/CalBasi.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Logar extends AppCompatActivity {
+public class CalBasi extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logar);
+        setContentView(R.layout.activity_calbasi);
     }
 }
