0d1a5e393c3586f473bf244b37e8ab4beae92d1e
Sollarium <caiorlima22@gmailc.om>
2023-03-09 12:33:08 -0300

    Adicionado a função de ida a todos os menus do menu principal, arrumado erro, dos Cal

--- app/src/main/java/com/example/aulaspdm/CalT.java
+++ app/src/main/java/com/example/aulaspdm/CalT.java
@@ -9,6 +9,7 @@ public class CalT extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cal);
+        setContentView(R.layout.activity_calt);
     }
+
 }
