ced3c8d47f1fa82c8ae75c123f0e2b36b13ebaff
Soll <caiorlima22@gmail.com>
2023-03-16 12:38:35 -0300

    Adicionado o segundo edit text.

--- app/src/main/java/com/example/aulaspdm/CalBasi.java
+++ app/src/main/java/com/example/aulaspdm/CalBasi.java
@@ -2,13 +2,32 @@ package com.example.aulaspdm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalBasi extends AppCompatActivity {
+    Intent it = new Intent();
+    EditText et1;
+    EditText et2;
+    TextView result;
+    Button mais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calbasi);
+
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        result = findViewById(R.id.tResult);
+        mais = findViewById(R.id.bNum9);
+
+
+
     }
+
+
 }
