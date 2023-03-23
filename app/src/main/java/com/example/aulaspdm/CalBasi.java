b3e2a2857bc06deced927c0f32b9f57a68fa79d1
Soll <caiorlima22@gmail.com>
2023-03-23 12:43:04 -0300

    Adicionado a calculadora basica, menos o botão voltar

--- app/src/main/java/com/example/aulaspdm/CalBasi.java
+++ app/src/main/java/com/example/aulaspdm/CalBasi.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,6 +16,9 @@ public class CalBasi extends AppCompatActivity {
     TextView result;
     Button mais;
 
+    Button menos;
+    Button div;
+    Button vezes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +27,65 @@ public class CalBasi extends AppCompatActivity {
         et1 = findViewById(R.id.et1);
         et2 = findViewById(R.id.et2);
         result = findViewById(R.id.tResult);
-        mais = findViewById(R.id.bNum9);
+        mais = findViewById(R.id.soma);
+        menos = findViewById(R.id.bNum11);
+        div = findViewById(R.id.bNum12);
+        vezes = findViewById(R.id.bNum13);
 
+
+            mais.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String teste = String.valueOf(et1.getText());
+                    String teste2 = String.valueOf(et2.getText());
+                    if (!teste.isEmpty() || !teste2.isEmpty()) {
+                        Double resulf = Double.parseDouble(String.valueOf(et1.getText())) + Double.parseDouble(String.valueOf(et2.getText()));
+                        result.setText("Resultado:" + resulf);
+                    }else {
+                        result.setText("Preencha os campos");
+                    }
+                }
+            });
+
+        menos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String teste = String.valueOf(et1.getText());
+                String teste2 = String.valueOf(et2.getText());
+                if (!teste.isEmpty() || !teste2.isEmpty()) {
+                    Double resulf = Double.parseDouble(String.valueOf(et1.getText())) - Double.parseDouble(String.valueOf(et2.getText()));
+                    result.setText("Resultado:" + resulf);
+                }else {
+                    result.setText("Preencha os campos");
+                }
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String teste = String.valueOf(et1.getText());
+                String teste2 = String.valueOf(et2.getText());
+                if (!teste.isEmpty() || !teste2.isEmpty()) {
+                    Double resulf = Double.parseDouble(String.valueOf(et1.getText())) / Double.parseDouble(String.valueOf(et2.getText()));
+                    result.setText("Resultado:" + resulf);
+                }else {
+                    result.setText("Preencha os campos");
+                }
+            }
+        });
+        vezes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String teste = String.valueOf(et1.getText());
+                String teste2 = String.valueOf(et2.getText());
+                if (!teste.isEmpty() || !teste2.isEmpty()) {
+                    Double resulf = Double.parseDouble(String.valueOf(et1.getText())) * Double.parseDouble(String.valueOf(et2.getText()));
+                    result.setText("Resultado:" + resulf);
+                }else {
+                    result.setText("Preencha os campos");
+                }
+            }
+        });
 
 
     }
